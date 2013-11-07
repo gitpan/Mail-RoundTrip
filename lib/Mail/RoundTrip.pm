1bd7b03961fbdc65de97ec0a2f45c8964a9c97cd
Chris Travers <chris.travers@gmail.com>
2013-11-07 04:37:16 +0000

    Import of EINHVERFR/Mail-RoundTrip-0.02 from CPAN.
    gitpan-cpan-distribution: Mail-RoundTrip
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         EINHVERFR/Mail-RoundTrip-0.02.tar.gz
    gitpan-cpan-author:       EINHVERFR
    gitpan-cpan-maturity:     released

--- lib/Mail/RoundTrip.pm
+++ lib/Mail/RoundTrip.pm
@@ -18,11 +18,11 @@ Mail::RoundTrip - Management routines for round trip validation of users' emails
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
